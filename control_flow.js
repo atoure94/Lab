02e8b7941bb3a48109ffa996553e468f7e749450
atoure94 <ahmad.alkawssar.toure@horizon-tech.tn>
2024-01-31 03:38:19 -0500

    functionjs

---
+++ control_flow.js
@@ -0,0 +1,16 @@
+let userType = "subscriber";
+let userCategory;
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+console.log("User Category:", userCategory);
